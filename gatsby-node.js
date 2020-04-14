314d49cf6ad7f17f9a6ff6ad09e977728d182bf8
Maggie <maggie@egghead.io>
2020-04-14 23:37:30 +0100

    reformatting

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,22 +4,9 @@ const _ = require('lodash')
 //Defining the createPosts function. It takes two callback functions – createPage() and createRedirect() – and an array of edges
 
 // const createPosts = (createPage, createRedirect, edges) => {
-// if (edges.node.fields.redirects) {
-//   node.fields.redirects.forEach(fromPath => {
-//     createRedirect({
-//       fromPath,
-//       toPath: pagePath,
-//       redirectInBrowser: true,
-//       isPermanent: true,
-//     })
-//   })
-// }
 
 //   if (edges.node.fields.categories === 'notes') {
-//     edges.forEach(({ node }, i) => {
-//       const pagePath = node.fields.slug
-//       const prevPage = i === 0 ? null : edges[i - 1].node
-//       const nextPage = i === edges.length - 1 ? null : edges[i + 1].node
+//
 //       createPage({
 //         path: pagePath,
 //         component: path.resolve('./src/templates/noteTemplate.js'),
@@ -69,12 +56,6 @@ const _ = require('lodash')
 
 exports.createPages = ({ actions, graphql }) => {
   const { createRedirect, createPage } = actions
-
-  // const notesTemplate = path.resolve('./src/templates/noteTemplate.js')
-  // const bookTemplate = path.resolve('./src/templates/bookTemplate.js')
-  // const illustrationTemplate = path.resolve(
-  //   './src/templates/illustrationTemplate.js',
-  // )
 
   return graphql(`
     query {
@@ -164,38 +145,48 @@ exports.createPages = ({ actions, graphql }) => {
       return Promise.reject('There are no posts!')
     }
 
-    data.notesQuery.edges.forEach(({ node }) => {
+    const pageRedirects = node => {
+      if (node.fields.redirects) {
+        node.fields.redirects.forEach(fromPath => {
+          createRedirect({
+            fromPath,
+            toPath: node.fields.slug,
+            redirectInBrowser: true,
+            isPermanent: true,
+          })
+        })
+      }
+    }
+
+    data.notesQuery.edges.forEach(({ node }, i) => {
+      pageRedirects(node)
       createPage({
         path: node.fields.slug,
         component: path.resolve('./src/templates/noteTemplate.js'),
         context: {
           id: node.id,
-          // prevPage,
-          // nextPage,
         },
       })
     })
 
     data.illustrationQuery.edges.forEach(({ node }) => {
+      pageRedirects(node)
       createPage({
         path: node.fields.slug,
         component: path.resolve('./src/templates/illustrationTemplate.js'),
         context: {
           id: node.id,
-          // prevPage,
-          // nextPage,
         },
       })
     })
 
     data.bookQuery.edges.forEach(({ node }) => {
+      pageRedirects(node)
       createPage({
         path: node.fields.slug,
         component: path.resolve('./src/templates/bookTemplate.js'),
         context: {
           id: node.id,
-          // prevPage,
-          // nextPage,
         },
       })
     })
