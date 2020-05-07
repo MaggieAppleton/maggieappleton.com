8a9fbb1681bcdc639d501d5b279ca585155f2a64
Maggie <maggie@egghead.io>
2020-05-07 16:31:40 +0100

    adding an essays page

---
+++ src/pages/essays.js
@@ -0,0 +1,135 @@
+import React from 'react'
+import Layout from 'components/Layout'
+import { Link } from 'gatsby'
+import Img from 'gatsby-image'
+import { css } from '@emotion/core'
+import { rhythm } from '../lib/typography'
+import { fonts } from '../lib/typography'
+import { useTheme } from 'components/Theming'
+import Container from 'components/Container'
+import { graphql } from 'gatsby'
+import SimpleCard from '../components/SimpleCard'
+
+const EssaysPage = ({ data: { site, essaysQuery } }) => {
+  const theme = useTheme()
+  return (
+    <Layout site={site}>
+      <Container
+        css={css`
+          margin-top: 1.6em;
+          margin-bottom: 2em;
+          display: grid;
+          grid-template-columns: auto-fill(4, minmax 200px, 1fr);
+          grid-column-gap: 1em;
+          .header {
+            grid-column-start: 1;
+            grid-column-end: 3;
+            margin-bottom: 1em;
+            max-width: 700px;
+            h1 {
+              margin-bottom: 0.4em;
+            }
+          }
+        `}
+      >
+        <section className="header">
+          <h1>Essays</h1>
+          <p>Coherent thoughts in essay format. Mostly illustrated.</p>
+        </section>
+
+        {/* ----------- Essays Section ----------- */}
+
+        <section>
+          <h2>Essays</h2>
+          <div>
+            {essaysQuery.edges.map(({ node: essay }) => (
+              <Link
+                to={essay.frontmatter.slug}
+                aria-label={`View ${essay.frontmatter.title}`}
+              >
+                <SimpleCard
+                  hover
+                  key={essay.id}
+                  css={css`
+                    font-family: ${fonts.regularSans};
+                    flex: 1 1 auto;
+                    max-width: 340px;
+                    margin-bottom: 1em;
+                    margin-right: 1em;
+                    padding: 0.6em 1.4em 1.6em 1.4em;
+                    h4 {
+                      font-size: 1.1em;
+                      margin-top: 0.2em;
+                      transition: all 150ms ease;
+                      &:hover: {
+                        color: ${theme.colors.primary};
+                      }
+                    }
+                    h5 {
+                      margin-bottom: 0;
+                    }
+                  `}
+                >
+                  <Img fluid={essay.frontmatter.cover.childImageSharp.fluid} />
+                  <h4>➽ {essay.frontmatter.title}</h4>
+                  <h5>{essay.frontmatter.description}</h5>
+                </SimpleCard>
+              </Link>
+            ))}
+          </div>
+        </section>
+      </Container>
+    </Layout>
+  )
+}
+
+export default EssaysPage
+
+export const EssaysPageQuery = graphql`
+  query {
+    site {
+      ...site
+      siteMetadata {
+        title
+      }
+    }
+
+    essaysQuery: allMdx(
+      filter: {
+        frontmatter: { categories: { eq: "essay" }, published: { ne: false } }
+      }
+      sort: { order: DESC, fields: frontmatter___date }
+      limit: 20
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 120)
+          id
+          fields {
+            title
+            slug
+            date
+          }
+          parent {
+            ... on File {
+              sourceInstanceName
+            }
+          }
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            description
+            slug
+            cover {
+              childImageSharp {
+                fluid(maxWidth: 300) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
--- src/pages/garden.js
+++ src/pages/garden.js
@@ -10,7 +10,7 @@ import Container from 'components/Container'
 import { graphql } from 'gatsby'
 import SimpleCard from '../components/SimpleCard'
 
-const GardenPage = ({ data: { site, notesQuery, essaysQuery } }) => {
+const GardenPage = ({ data: { site, notesQuery } }) => {
   const theme = useTheme()
   return (
     <Layout site={site}>
@@ -18,28 +18,17 @@ const GardenPage = ({ data: { site, notesQuery, essaysQuery } }) => {
         css={css`
           margin-top: 1.6em;
           margin-bottom: 2em;
-          display: grid;
-          grid-template-columns: 30% 70%;
-          grid-column-gap: 1em;
           .header {
-            grid-column-start: 1;
-            grid-column-end: 3;
             margin-bottom: 1em;
             max-width: 700px;
             h1 {
               margin-bottom: 0.4em;
             }
           }
-          .essays {
-            grid-column: 2 / 2;
-            .essaysGrid {
-              display: flex;
-              flex-direction: row;
-              flex-wrap: wrap;
-            }
-          }
-          .notes {
-            grid-column: 1/ 1;
+          .notesGrid {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
           }
         `}
       >
@@ -62,55 +51,14 @@ const GardenPage = ({ data: { site, notesQuery, essaysQuery } }) => {
               >
                 <h4
                   css={css`
-                    max-width: 300px;
+                    width: 260px;
                     margin-top: 0.4em;
+                    margin-right: 1.8em;
                   `}
                   key={note.id}
                 >
                   {note.frontmatter.title}
                 </h4>
-              </Link>
-            ))}
-          </div>
-        </section>
-
-        {/* ----------- Essays Section ----------- */}
-
-        <section className="essays">
-          <h2>Essays</h2>
-          <div className="essaysGrid">
-            {essaysQuery.edges.map(({ node: essay }) => (
-              <Link
-                to={essay.frontmatter.slug}
-                aria-label={`View ${essay.frontmatter.title}`}
-              >
-                <SimpleCard
-                  hover
-                  key={essay.id}
-                  css={css`
-                    font-family: ${fonts.regularSans};
-                    flex: 1 1 auto;
-                    max-width: 340px;
-                    margin-bottom: 1em;
-                    margin-right: 1em;
-                    padding: 0.6em 1.4em 1.6em 1.4em;
-                    h4 {
-                      font-size: 1.1em;
-                      margin-top: 0.2em;
-                      transition: all 150ms ease;
-                      &:hover: {
-                        color: ${theme.colors.primary};
-                      }
-                    }
-                    h5 {
-                      margin-bottom: 0;
-                    }
-                  `}
-                >
-                  <Img fluid={essay.frontmatter.cover.childImageSharp.fluid} />
-                  <h4>➽ {essay.frontmatter.title}</h4>
-                  <h5>{essay.frontmatter.description}</h5>
-                </SimpleCard>
               </Link>
             ))}
           </div>
@@ -158,44 +106,6 @@ export const GardenPageQuery = graphql`
             description
             slug
             topics
-          }
-        }
-      }
-    }
-
-    essaysQuery: allMdx(
-      filter: {
-        frontmatter: { categories: { eq: "essay" }, published: { ne: false } }
-      }
-      sort: { order: DESC, fields: frontmatter___date }
-      limit: 12
-    ) {
-      edges {
-        node {
-          excerpt(pruneLength: 120)
-          id
-          fields {
-            title
-            slug
-            date
-          }
-          parent {
-            ... on File {
-              sourceInstanceName
-            }
-          }
-          frontmatter {
-            title
-            date(formatString: "MMMM DD, YYYY")
-            description
-            slug
-            cover {
-              childImageSharp {
-                fluid(maxWidth: 300) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
           }
         }
       }
