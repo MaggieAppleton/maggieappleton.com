699b1663431739ba1fa90234230097a04ea739dc
Maggie <maggie@egghead.io>
2020-05-09 22:35:11 +0100

    finished the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,8 @@ import Img from 'gatsby-image'
 import Container from 'components/Container'
 import { useStaticQuery, graphql } from 'gatsby'
 import { css } from '@emotion/core'
+import { bpMaxSM } from '../lib/breakpoints'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
   const data = useStaticQuery(graphql`
@@ -16,14 +18,14 @@ const AboutPage = () => {
       }
       profilePic: file(relativePath: { eq: "profilePic.png" }) {
         childImageSharp {
-          fluid(maxWidth: 480) {
+          fluid(maxWidth: 600) {
             ...GatsbyImageSharpFluid
           }
         }
       }
       venn: file(relativePath: { eq: "venndiagram.png" }) {
         childImageSharp {
-          fluid(maxWidth: 330) {
+          fluid(maxWidth: 450) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -31,18 +33,34 @@ const AboutPage = () => {
     }
   `)
 
-  console.log(data.profilePic.childImageSharp.fluid)
-
   return (
     <Layout site={data.site}>
       <Container
         css={css`
+          justify-items: center;
           margin-top: 4em;
           max-width: 80%;
           .header {
             display: grid;
-            grid-template-columns: 40% 60%;
-            grid-gap: 4em;
+            grid-template-columns: 45% 65%;
+            grid-gap: 3.2em;
+            ${bpMaxSM} {
+              grid-template-columns: 1fr;
+            }
+            .headerText {
+              max-width: 540px;
+              h2 {
+                opacity: 80%;
+                max-width: 500px;
+                font-size: 1.7em;
+                line-height: 1.3em;
+                margin-bottom: 1em;
+              }
+              p {
+                max-width: 500px;
+                line-height: 1.7em;
+              }
+            }
           }
         `}
       >
@@ -54,23 +72,44 @@ const AboutPage = () => {
             }}
             fluid={data.profilePic.childImageSharp.fluid}
           />
-          <div>
+          <div className="headerText">
             <h1>Maggie Appleton</h1>
-            <h3>
+            <h2>
               Anthropologist, art director, illustrator, metaphor designer, and
               mediocre developer
-            </h3>
+            </h2>
+            <Img
+              style={{
+                maxWidth: '420px',
+                margin: '0 auto',
+                marginBottom: '1em',
+              }}
+              fluid={data.venn.childImageSharp.fluid}
+            />
             <p>
-              I sit in the middle of an odd venn diagram – illustration,
-              anthropology, and technology are at the core of everything I make.
+              I sit in the middle of a few odd venn diagrams – illustration,
+              anthropology, and technology are at the core of everything I make.{' '}
+            </p>
+            <p>
               Combining these into something coherent is a weird and ongoing
               challenge.
             </p>
-            <Img
-              objectFit="contain"
-              objectPosition="0% 0%"
-              fluid={data.venn.childImageSharp.fluid}
-            />
+            <p>
+              My work at <a href="https://egghead.io/">egghead</a> involves
+              designing visual metaphors for invisible, complicated software
+              concepts.
+            </p>
+            <p>
+              On the side I'm always working on{' '}
+              <Link to="/essays">illustrated essays</Link> that explore the
+              cultural side of digital technology.
+            </p>
+            <p>
+              In my
+              <Link to="/garden"> digital garden of notes</Link> you'll find a
+              good amount of linguistics, cognitive metaphors, cultural
+              speculation, and tool nerding.
+            </p>
           </div>
         </div>
       </Container>
