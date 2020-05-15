ea8aa8965e6367085d0b0f1a46a8bf1633447fdf
Maggie <maggie@egghead.io>
2020-05-15 13:22:25 +0100

    temp fix remove transitions

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import SimpleTransition from './src/components/Transition'
+// import React from 'react'
+// // import SimpleTransition from './src/components/Transition'
 
-export const wrapPageElement = ({ element, props }) => {
-  return <SimpleTransition props={props}>{element}</SimpleTransition>
-}
+// export const wrapPageElement = ({ element, props }) => {
+//   return <script src="https://hypothes.is/embed.js" async></script>
+//   //   <SimpleTransition props={props}>{element}</SimpleTransition>
+// }
