f08e194181b1251aa66110bce79e971c5f0e8ffb
Maggie <maggie@egghead.io>
2020-05-16 23:05:41 +0100

    footnotes

--- src/components/mdx/Footnote.js
+++ src/components/mdx/Footnote.js
@@ -1,30 +1,57 @@
 import React from 'react'
 import { css } from '@emotion/core'
+import { bpMaxSM } from '../../lib/breakpoints'
 
-const Footnote = ({ children }) => {
+const Footnote = ({ count, children }) => {
   return (
-    <span
-      css={css`
-        position: relative;
-        float: right;
-        overflow: hidden;
-        box-sizing: content-box;
-        max-width: 240px;
-        font-size: 75%;
-        padding: 0.6em;
-        right: -10%;
-        margin-left: -10%;
-        &::after {
-          clear: both;
-          height: 0;
-          width: 100%;
-          content: '';
-          display: block;
-        }
-      `}
-    >
-      {children}
-    </span>
+    <>
+      <span
+        css={css`
+          counter-increment: MarginNote;
+          content: counter(MarginNote);
+          vertical-align: super;
+          line-height: 0;
+          font-size: 70%;
+        `}
+      >
+        {count}
+      </span>
+      <span
+        css={css`
+          position: relative;
+          float: right;
+          line-height: 1.4em;
+          overflow: hidden;
+          box-sizing: content-box;
+          max-width: 240px;
+          font-size: 75%;
+          padding: 1em;
+          right: -25%;
+          margin-left: -20%;
+          &::before {
+            content: counter(MarginNote);
+            width: 15px;
+            height: 20px;
+            position: absolute;
+            left: -0.5em;
+            text-align: right;
+            top: 0.6rem;
+          }
+          &::after {
+            clear: both;
+            height: 0;
+            width: 100%;
+            content: '';
+            display: block;
+          }
+          ${bpMaxSM} {
+            display: none;
+          }
+        `}
+      >
+        {children}
+      </span>
+    </>
   )
 }
 
