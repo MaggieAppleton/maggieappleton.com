d903763ef9e6d2d8ac339ca6d47962b698edf290
Maggie <maggie@egghead.io>
2020-06-06 01:24:25 +0100

    favicon update

--- config/website.js
+++ config/website.js
@@ -6,7 +6,7 @@ module.exports = {
   siteTitleShort: 'Maggie_Appleton', // short_name for manifest
   siteUrl: 'https://maggieappleton.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteLogo: 'images/favicon-ma.svg', // Used for SEO and manifest, path to your image you placed in the 'static' folder
   siteDescription:
     'Maggie Appleton is an art director, anthropologist, and metaphor-making illustrator. This is her digital garden for growing visual explanations about technology, culture, and programming',
   author: 'Maggie Appleton', // Author for schemaORGJSONLD
