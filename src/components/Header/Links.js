517b57f0ccef776b41cbb61eeffe6434719c8cc6
Maggie <maggie@egghead.io>
2020-06-09 21:31:06 +0100

    fixed broken gatsby-link component

--- src/components/Header/Links.js
+++ src/components/Header/Links.js
@@ -4,19 +4,19 @@ import { Link } from 'gatsby'
 export default () => {
   return (
     <span>
-      <Link to="./illustration" aria-label="View illustration page">
+      <Link to="/illustration" aria-label="View illustration page">
         Illustration
       </Link>
-      <Link to="./essays" aria-label="View essays page">
+      <Link to="/essays" aria-label="View essays page">
         Essays
       </Link>
-      <Link to="./garden" aria-label="View notes and essays page">
+      <Link to="/garden" aria-label="View notes and essays page">
         Digital Garden
       </Link>
-      <Link to="./bookshelf" aria-label="View books page">
+      <Link to="/bookshelf" aria-label="View books page">
         Bookshelf
       </Link>
-      <Link to="./about" aria-label="View about page">
+      <Link to="/about" aria-label="View about page">
         About
       </Link>
       {/* <Link
