1031b552c7b801913ca28d677ff6ba9ecc45279c
Maggie <maggie@egghead.io>
2020-06-10 15:46:39 +0100

    added google tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -130,7 +130,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `GOOGLE_ID`,
+        trackingId: `UA-45097160-1`,
       },
     },
     {
