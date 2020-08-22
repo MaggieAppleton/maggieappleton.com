bf31c2e233e8dd78cd3da0408dd1ab85d651fab5
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-08-22 21:14:47 +0100

    2% less shitty writing

--- src/components/mdx/SingleUse/MMMSection.js
+++ src/components/mdx/SingleUse/MMMSection.js
@@ -4,17 +4,7 @@ import { css } from '@emotion/core'
 
 // Make background of the main layout an image
 const mainLayout = css`
-left: 50%;
-margin-left: -50vw;
-margin-right: -50vw;
-max-width: 100vw;
-position: relative;
-right: 50%;
-width: 100vw;
-height: 100%;
-  background: offwhite;
   display: flex;
-  z-index: -1;
   .innerLayout {
     margin: 0 auto;
     padding: 2em 0;
@@ -26,19 +16,19 @@ height: 100%;
         margin: 0.6em auto 0.4em;
     }
     p {
-        max-width: 540px;
+        max-width: 550px;
         line-height: 1.75;
         z-index: 1;
     }
   }
 `
 
-export const MMMSection = ({ sectionTitle, description }) => {
+export const MMMSection = ({ sectionTitle, children }) => {
   return (
   <div css={mainLayout}>
       <div className='innerLayout'>
           <h1>{sectionTitle}</h1>
-          <div>{description}</div>
+          {children}
         </div>
     </div>
   )
--- src/components/mdx/Title.js
+++ src/components/mdx/Title.js
@@ -6,7 +6,7 @@ export default styled.h1`
   text-align: left;
   max-width: 660px;
   margin: 0 auto;
-  line-height: 1.1;
+  line-height: 1.2;
   margin-top: 1.4em;
   margin-bottom: 0.4em;
 `
