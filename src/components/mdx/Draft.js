b24663ff5016e0def1da5a2204376e6235c13a21
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-08-25 22:23:19 +0100

    fixed typos in draft

--- src/components/mdx/Draft.js
+++ src/components/mdx/Draft.js
@@ -15,7 +15,7 @@ export const Draft = () => {
       <span style={{ fontSize: '2.8em', marginTop: '0' }} role="img" aria-label="seedling">☢️</span>
       <h1 style={{ fontSize: '2.8em', marginTop: '0' }}>Draft in Progress</h1>
       <h5 style={{ marginBottom: '0.6em' }}>
-        The quality of writing on below this point is haphazard, disjointed, and possibly non-sensical. It's probably a good idea to come back later.
+        The quality of writing below this point is haphazard, disjointed, and possibly nonsensical. It's probably a good idea to come back later.
       </h5>
     </SimpleCard>
   )
