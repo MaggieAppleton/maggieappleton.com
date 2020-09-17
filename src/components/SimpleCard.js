95ce6d1f694dc5bc523395f292a137c1dc5d1f20
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-09-17 12:24:00 +0100

    edited simple card component

--- src/components/SimpleCard.js
+++ src/components/SimpleCard.js
@@ -13,12 +13,17 @@ const SimpleCard = props => {
           ? 'all 0.8s cubic-bezier(0.2, 0.8, 0.2, 1) 0s;'
           : null,
         margin: '0 auto',
+        clear: 'both',
         marginBottom: props.marginBottom ? props.marginBottom : '2em',
         marginTop: props.marginTop ? props.marginTop : null,
         padding: props.padding ? props.padding : '1.4em 1.6em 2.4em',
         width: props.width ? props.width : '100%',
         maxWidth: props.maxWidth ? props.maxWidth : '100%',
         borderRadius: '6px',
+        'p, h1, h2, h3, h4, button' : {
+          textAlign: props.center ? 'center' : null,
+          margin: props.center ? '0.8em auto 0' : null,
+        },
         boxShadow: props.hover
           ? '0px 1px 2px rgba(52, 61, 68, 0.1)'
           : '0 4px 10px -4px rgba(0,0,0,0.15)',
