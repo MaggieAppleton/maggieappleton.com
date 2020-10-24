081a40bfc10948c2d19d0f51e5a7d73ff6564d6f
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-10-24 11:12:19 +0100

    added gsap to gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import SimpleTransition from './src/components/Transition'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger);
+}
 
 export const wrapPageElement = ({ element, props }) => {
   return <SimpleTransition props={props}>{element}</SimpleTransition>
 }
+
 
   // return <script src="https://hypothes.is/embed.js" async></script>
