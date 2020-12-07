0f62a43760de57bb7bfdcabbe6493115a7e89602
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-12-07 17:26:53 +0000

    basic styling

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -3,8 +3,8 @@ import styled from '@emotion/styled'
 export const Tabs = styled.div`
     overflow: hidden;
     height: auto;
-    width: 100%;
-    margin-top: 2.5em;
+    width: 95%;
+    margin: 2.5em auto 0;
     display: flex;
     justify-content: space-around;
 `
@@ -15,10 +15,11 @@ export const Tab = styled.button`
   outline: none;
   cursor: pointer;
   position: relative;
-  color: black;
-  border: ${props => (props.activeTab ? "1px solid #ccc" : "")};
-  border-bottom: ${props => (props.activeTab ? "none" : "")};
-  background-color: ${props => (props.activeTab ? "blue" : "white")};
+  color: ${props => (props.activeTab ? "orange" : "gray")};
+  border-radius: 0;
+  border: none;
+  border-bottom: ${props => (props.activeTab ? "3px solid orange" : "1px solid lightgray")};
+  background-color: white;
   height: ${props => (props.activeTab ? "3em" : "2.6em; top:.4em")};
   transition: background-color 0.5s ease-in-out;
   :hover {
