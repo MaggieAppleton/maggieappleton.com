2e40caab05d9f132cc22b68f30890c046f0e4758
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-12-09 20:06:03 +0000

    acceptable styling level on tabs

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,9 +1,10 @@
+import colors from '../lib/colors'
 import styled from '@emotion/styled'
 
 export const Tabs = styled.div`
     overflow: hidden;
     height: auto;
-    width: 95%;
+    width: 75%;
     margin: 2.5em auto 0;
     display: flex;
     justify-content: space-around;
@@ -15,15 +16,21 @@ export const Tab = styled.button`
   outline: none;
   cursor: pointer;
   position: relative;
-  color: ${props => (props.activeTab ? "orange" : "gray")};
+  font-size: 90%;
+  color: ${props => (props.activeTab ? colors.orange : colors.grey)};
   border-radius: 0;
   border: none;
-  border-bottom: ${props => (props.activeTab ? "3px solid orange" : "1px solid lightgray")};
+  border-bottom: ${props => (props.activeTab ? `2px solid ${colors.lightOrange}` : `1px solid ${colors.lightestGrey}`)};
   background-color: white;
   height: ${props => (props.activeTab ? "3em" : "2.6em; top:.4em")};
-  transition: background-color 0.5s ease-in-out;
+  transition: all 200ms ease-in-out;
   :hover {
     background-color: white;
+    border: none;
+    border-bottom: 1px solid ${colors.lightestGrey};
+    color: ${colors.orange};
+    box-shadow: none;
+    transform: none;
   }
 `
 
