c2b383217004f21e4467cc078590207bc2466ce7
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-01-01 22:09:51 +0000

    made stream animation

--- src/components/mdx/SingleUse/StreamAnimation.js
+++ src/components/mdx/SingleUse/StreamAnimation.js
@@ -18,7 +18,7 @@ export const StreamAnimation = () => {
         scrollTrigger: {
             trigger: [triggerDiv.current],
             markers: true,
-            start: '-220% 30%',
+            start: '-220% 50%',
             end: '-140% 30%',
             scrub: true
         },
@@ -26,36 +26,54 @@ export const StreamAnimation = () => {
     })
 
     timeline
-    .set([box1.current], {yPercent: -15} )
-    .set([box2.current], {yPercent: -15} )
-    .set([box3.current], {yPercent: -15} )
-    .set([box4.current], {yPercent: -15} )
+    .set([box1.current], {yPercent: -10} )
+    .set([box2.current], {yPercent: -200} )
+    .set([box3.current], {yPercent: -300} )
+    .set([box4.current], {yPercent: -400} )
     .to([box1.current], {
         'fill-opacity': 1,
         yPercent: 0, scaleY: '1.1', ease: "sine.inOut", duration: '0.6'
     })
+    .to([box1.current], {
+        yPercent: 120, scaleY: '1.1', ease: "sine.inOut"
+    }, 1)
     .to([box2.current], {
         'fill-opacity': 1,
-        yPercent: 0, scaleY: '1.1', ease: "sine.inOut", duration: '0.6'
-    })
+        yPercent: -110, scaleY: '1.1', ease: "sine.inOut", duration: '0.6'
+    }, 1)
+    .to([box1.current], {
+        yPercent: 240, scaleY: '1.1', ease: "sine.inOut", duration: '0.6'
+    }, 1.5)
+    .to([box2.current], {
+        yPercent: 10, scaleY: '1.1', ease: "sine.inOut", duration: '0.6'
+    }, 1.5)
     .to([box3.current], {
         'fill-opacity': 1,
-        yPercent: 0, scaleY: '1.1', ease: "sine.inOut", duration: '0.6'
-    })
+        yPercent: -220, scaleY: '1.1', ease: "sine.inOut", duration: '0.6'
+    }, 2)
+    .to([box1.current], {
+        yPercent: 360, scaleY: '1.1', ease: "sine.inOut", duration: '0.6'
+    }, 2.5)
+    .to([box2.current], {
+        yPercent: 130, scaleY: '1.1', ease: "sine.inOut", duration: '0.6'
+    }, 2.5)
+    .to([box3.current], {
+        yPercent: -100, scaleY: '1.1', ease: "sine.inOut", duration: '0.6'
+    }, 2.5)
     .to([box4.current], {
         'fill-opacity': 1,
-        yPercent: 6, scaleY: '1.1', ease: "sine.inOut", duration: '0.6'
-    })
+        yPercent: -385, scaleY: '1.1', ease: "sine.inOut", duration: '0.6'
+    }, 2.5)
 }, [])
 
   return (
     <div style={{ width: '60%', marginTop: '2em' }} ref={triggerDiv}>
         <svg width="341" height="550" viewBox="0 0 341 550" fill="none" xmlns="http://www.w3.org/2000/svg">
         
-        <rect ref={box1} width="341" height="104" fill="#C4C4C4"fill-opacity="0" />
-        <rect ref={box2} y="119" width="341" height="104" fill="#C4C4C4" fill-opacity="0" />
-        <rect ref={box3} y="238" width="341" height="162" fill="#C4C4C4" fill-opacity="0" />
-        <rect ref={box4} y="415" width="341" height="135" fill="#C4C4C4"fill-opacity="0" />
+        <rect ref={box1} width="341" height="105" fill="#C4C4C4"fill-opacity="0" />
+        <rect ref={box2} y="119" width="341" height="105" fill="#C4C4C4" fill-opacity="0" />
+        <rect ref={box3} y="238" width="341" height="105" fill="#C4C4C4" fill-opacity="0" />
+        <rect ref={box4} y="415" width="341" height="105" fill="#C4C4C4"fill-opacity="0" />
         
         </svg>
     </div>
