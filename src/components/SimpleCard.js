9dce2de33867755d1e20fcf21d95cfd1baf5da4c
Miles Crighton <miles.crighton@gmail.com>
2021-03-20 10:13:24 +0000

    fix: Animate pseudo-element instead of border

--- src/components/SimpleCard.js
+++ src/components/SimpleCard.js
@@ -8,6 +8,7 @@ const SimpleCard = props => {
   return (
     <div
       css={css({
+        position: 'relative',
         gridColumn: '1/4',
         border: '1px solid rgba(52, 61, 68, 0.05)',
         transition: props.hover
@@ -21,7 +22,7 @@ const SimpleCard = props => {
         width: props.width ? props.width : '100%',
         maxWidth: props.maxWidth ? props.maxWidth : '100%',
         borderRadius: '6px',
-        'p, h1, h2, h3, h4, h5, h6, span, button' : {
+        'p, h1, h2, h3, h4, h5, h6, span, button': {
           textAlign: props.textcenter ? 'center' : null,
           margin: props.center ? '0.8em auto 0' : null,
         },
@@ -30,14 +31,25 @@ const SimpleCard = props => {
           : '0 4px 10px -4px rgba(0,0,0,0.15)',
         ':hover': {
           transform: props.hover ? 'scale(1.015)' : null,
-          borderTop: props.hover
-            ? `2px solid ${theme.colors.lightOrange}`
-            : null,
           borderRadius: props.hover ? '0px 0px 6px 6px' : null,
           boxShadow: props.hover ? '0 10px 30px -10px rgba(0,0,0,0.15)' : null,
           p: {
             transition: 'all 0.8s cubic-bezier(0.2, 0.8, 0.2, 1) 0s;',
           },
+          '::before': {
+            backgroundColor: props.hover ? theme.colors.lightOrange : null,
+          },
+        },
+        '::before': {
+          position: 'absolute',
+          content: '" "',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '2px',
+          backgroundColor: 'transparent',
+          transition:
+            'background-color 0.8s cubic-bezier(0.2, 0.8, 0.2, 1) 0s;',
         },
       })}
       {...props}
