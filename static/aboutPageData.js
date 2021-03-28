9eaf7939b78011c54954ec2d73639a3637c50e8b
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-03-28 16:10:50 +0100

    added talks and podcasts data

---
+++ static/aboutPageData.js
@@ -0,0 +1,65 @@
+export const podcastData = [
+    {
+        podcastName: 'The Swyx Mixtape',
+        episodeName: 'Growing Digital Gardens and Tending the Web',
+        date: 'Feb 13 2021',
+        url: 'https://podcasts.apple.com/us/podcast/weekend-drop-digital-gardening-w-maggie-appleton/id1549059398?i=1000508923478'
+    },
+    {
+        podcastName: 'RoamFM',
+        episodeName: 'Digital Anthropology, Digital Gardens and Illustrated Notes',
+        date: 'July 25 2020',
+        url: 'https://thatsthenorm.com/maggie-appleton-transcript/'
+    },
+    {
+        podcastName: 'egghead.io Podcast',
+        episodeName: '#43 Turning Technical Concepts into Approachable Illustrated Metaphors',
+        date: 'May 3, 2019',
+        url: 'https://egghead.io/podcasts/turning-technical-concepts-into-approachable-illustrated-metaphors-with-maggie-appleton'
+    },
+    {
+        podcastName: 'Maintainers Anonymous',
+        episodeName: 'Embodiment Through Metaphors',
+        date: 'May 13 2020',
+        url: 'https://maintainersanonymous.com/metaphor/'
+    },
+    {
+        podcastName: 'Maintainers Anonymous',
+        episodeName: 'Open Source as a Gift Economy',
+        date: 'March 5 2020',
+        url: 'https://maintainersanonymous.com/gift/'
+    },
+    {
+        podcastName: 'React Podcast',
+        episodeName: '#86 The Power of Mental Model‪s',
+        date: 'March 3, 2020',
+        url: 'https://reactpodcast.simplecast.com/episodes/86'
+    },
+]
+
+export const talksData = [
+    {
+        talkTitle: 'Drawing the Invisible: React Explained in Five Visual Metaphors',
+        date: 'September 2020',
+        conference: 'egghead Talks',
+        url: 'https://egghead.io/talks/javascript-drawing-the-invisible-react-explained-in-five-visual-metaphors'
+    },
+    {
+        talkTitle: 'The Cultural Anthropology of React',
+        date: 'August 2020',
+        conference: 'React Rally',
+        url: 'https://maggieappleton.com/anthropology-react'
+    },
+    {
+        talkTitle: 'Evergreen Notes and Digital Gardens',
+        date: 'May 2020',
+        conference: 'Roam Research Tours',
+        url: 'https://www.youtube.com/watch?v=RXXXHN516qc'
+    },
+    {
+        talkTitle: 'Drawing the Invisible: React Explained in Five Visual Metaphors',
+        date: 'May 2020',
+        conference: 'Women of React',
+        url: 'https://www.youtube.com/watch?v=MQGe9zxlRdk'
+    },
+]
