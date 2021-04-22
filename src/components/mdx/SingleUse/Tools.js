8e58fc328cfdbf72143f4f3cdbf1cfb34c4d1ca2
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-04-22 23:08:13 +0100

    fixed spacing on what apps #IndieGardening

--- src/components/mdx/SingleUse/Tools.js
+++ src/components/mdx/SingleUse/Tools.js
@@ -4,17 +4,21 @@ import { bpMaxLG, bpMaxMD } from '../../../lib/breakpoints'
 
 const mainLayout = css`
   grid-column: 1/4;
-  max-width: 800px;
+  max-width: 700px;
   display: flex;
   flex-direction: row;
   align-content: center;
   align-items: center;
   text-align: center;
   justify-content: center;
-  margin: 1.4em auto;
+  margin: 1.8em auto 0;
+  a {
+    max-width: 200px;
+  }
   img {
     transition: all 400ms ease;
     max-width: 120px;
+    margin-bottom: 0;
     &:hover {
       transition: all 400ms ease;
       transform: translateY(-6px);
@@ -23,9 +27,10 @@ const mainLayout = css`
   h5 {
     transition: all 400ms ease;
     text-align: center;
-    align-self: center;
-    margin-top: 0.4em;
     font-size: 1em;
+    max-width: 200px;
+    min-width: 200px;
+    margin-bottom: 1.6em;
   }
   ${bpMaxMD} {
     flex-wrap: wrap;
@@ -36,8 +41,17 @@ const largeImages = css`
   ${bpMaxLG} {
     flex-wrap: wrap;
   }
+  a {
+    max-width: 400px;
+  }
   img {
     max-width: 400px;
+    padding: 0 1em;
+
+  }
+  h5 {
+    max-width: 400px;
+    min-width: 400px;
   }
 `
 
